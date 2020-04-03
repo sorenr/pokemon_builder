c09dace2c9d6e6efe0b95e76486c9703ebf59a55
Søren Ragsdale <sorenr@users.noreply.github.com>
2020-04-03 13:50:46 +0100

    optimal_table.py: more efficient multiprocessing

--- optimal_table.py
+++ optimal_table.py
@@ -1,51 +1,83 @@
 #!/usr/bin/env python3
 
 import sys
+import argparse
+import logging
 import multiprocessing
 
 import game_master
 import pokemon
 
-
 """Precompute a JSON table of optimal PvP IVs."""
 
 
-def find_optimal_proc(name):
-    """Worker process to compute optimal IVs for 1500 and 2500 cp"""
-    p = pokemon.Pokemon(GM, name=name)
-    optimal = {}
-    for mcp in [1500, 2500, None]:
-        o = p.optimize_iv(max_cp=mcp)
-        if o:
-            optimal.setdefault(name, {})[mcp] = o
-    return optimal
+class IVOptimizer():
+    def __init__(self):
+        self.pokemon = pokemon.Pokemon(GM)
+
+    def find_optimal_proc(self, names):
+        """Worker process to compute optimal IVs for 1500 and 2500 cp"""
+        optimal = {}
+        for name in names:
+            self.pokemon.update(name=name)
+            for mcp in [1500, 2500, None]:
+                o = self.pokemon.optimize_iv(max_cp=mcp)
+                if o:
+                    optimal.setdefault(name, {})[mcp] = o
+        return optimal
 
 
-def find_optimal_multi(out=pokemon.OPTIMAL_IV):
+def chunk(l, n):
+    """Divide a list into n-sized chunks."""
+    n = max(1, n)
+    for i in range(0, len(l), n):
+        yield l[i: i + n]
+
+
+def find_optimal_multi(iv_cache=pokemon.OPTIMAL_IV, threads=None):
     """Coordinating process to run find_optimal_proc across multiple procs"""
-    optimal = pokemon.Cache(out)
+    optimizer = IVOptimizer()
+    iv_cache = pokemon.Cache(iv_cache)
     # Find which pokemon are not in the list
-    missing = [x for x in GM.pokemon.keys() if x not in optimal]
-    print("Computing optimal IVs for", len(missing), "pokemon")
-    with multiprocessing.Pool() as pool:
-        try:
-            for rv in pool.imap_unordered(find_optimal_proc, missing):
-                optimal.update(rv)
-                sys.stdout.write(".")
-                sys.stdout.flush()
-        except KeyboardInterrupt:
-            pass
+    missing = [x for x in GM.pokemon.keys() if x not in iv_cache]
+    print("Computing optimal IVs for", len(missing), "pokemon using", threads, threads > 1 and "threads" or "thread")
+    try:
+        if threads == 1:
+            # just run serially
+            pokemon.Pokemon.iv_cache.update(optimizer.find_optimal_proc(missing))
+        else:
+            missing = chunk(missing, 20)
+            with multiprocessing.Pool(threads) as pool:
+                for rv in pool.imap_unordered(optimizer.find_optimal_proc, missing):
+                    iv_cache.update(rv)
+                    sys.stdout.write(".")
+                    sys.stdout.flush()
+    except KeyboardInterrupt:
+        pass
     sys.stdout.write("\n")
-    optimal.write()
-    return optimal
+    iv_cache.write()
+    return iv_cache
 
 
 if __name__ == "__main__":
-    GM = game_master.GameMaster()
-    if len(sys.argv) > 1:
-        optimal = find_optimal_multi(sys.argv[1])
+    """Not intended for standalone use."""
+    parser = argparse.ArgumentParser(description='Retrieve and parse a GAME_MASTER.json file')
+    parser.add_argument("-v", dest="verbose", help="verbose output", action="store_true")
+    parser.add_argument("-t", dest="threads", help="threads", type=int, default=multiprocessing.cpu_count())
+    parser.add_argument("-o", dest="output", help="output JSON file", default=pokemon.OPTIMAL_IV)
+    args = parser.parse_args()
+
+    if args.verbose:
+        log_level = logging.DEBUG
+        log_format = "%(levelname)-5s %(funcName)s:%(lineno)d > %(message)s\n"
     else:
-        optimal = find_optimal_multi()
+        log_level = logging.INFO
+        log_format = "%(message)s"
+
+    logging.basicConfig(level=log_level, format=log_format)
+
+    GM = game_master.GameMaster()
+    optimal = find_optimal_multi(args.output, threads=args.threads)
 
     # reorder by highest stat product
     best = {}
